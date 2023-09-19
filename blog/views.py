a2ac4ecd92d6f50554941af862e6d912454f51bd
unknown <softech.kevin@gmail.com>
2023-09-19 11:29:30 -0500

    first commit

---
+++ blog/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, get_object_or_404
+from .models import post
+
+
+
+def render_posts(request):
+    posts = post.objects.all()
+    return render(request, 'posts.html', {'posts': posts})
+
+def post_detail(request, post_id):
+    post_id=get_object_or_404(post, pk=post_id)
+    return render(request, 'post_detail.html', {"post": post_id})
